46dae313dd2fda9ecdddc1593e281fa5e5c7d27f
Eddy Varela <edv2@williams.edu>
2020-05-23 17:37:32 -0400

    added homepage forms and placeholder image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
-import {Container, Col} from 'reactstrap';
+import {Container, Col, Row} from 'reactstrap';
+import dnaImage from '../assets/dnaHead.jpg';
 
 const About = () => {
     return (
         <Container>
-            <h1>
-                About Genesim
-            </h1>
-            
-            <Col lg = '6' >
+            <br/>
+            <Row>
+                <Col lg = '6' >
+                <h1>
+                    About Genesim
+                </h1>
                 <p> 
                     Genesim was inspire by a DNA + Latinx class at Williams College.
                     This course built the scientific foundations and historical context around DNA then proceeded
-                    to show how powerful genetic code is. Computational Biology is an entire new field of research
-                    that exists at the intersection of genetics, computer science, and math/statistics that focuses
-                    on analyzing the human genome to find certain special insights.
+                    to show how powerful genetic code is. Since genetic code represents different illnesses, fields like
+                    Computational Biology have brought incredible insights into epidemiology, oncology research, and degenerative diseases.
+                    <br/>
+                    <br/>
+                    
+                    This project is an attempt to simulate a gene selection process and capture the intersection between
+                    genetics, anthropology, computer science, and math/statistics. The avatars generated will have a 
+                    short history snippet attached to them based on the features selected and their country of origin. 
+
                     
                     For example, much of the cancer research is  if From the basic building blocks to sequence someone's
                     appearance, medical conditions, or special 
@@ -23,6 +31,27 @@ const About = () => {
                     highlighted the scientific backgroun of DNA 
                 </p>
                 </Col>
+                <Col lg = '6'>
+                    <img src = {dnaImage}></img>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg  = '6'>
+                    <h2>Future work</h2>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col lg = '6'>
+                <h2>About the author</h2>
+                    <p1> My name is Eddy Varela and I am a software developer with a computer science and math major, however,
+                        I also love storytelling, filmmaking, and video. I am interested in how people decide which traits are
+                        more favorable in terms of eye color, skin complexion, hair, nose, etc and how this plays out in their
+                        lived experience. Many narratives that we have seen in the course heavily revolve around physical
+                        appearance defining a person’s country of origin and their lived experience.</p1>
+                </Col>
+            </Row>
+            
         </Container>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Container, Label, Input, FormGroup, Button} from 'reactstrap';
+import { Container, Label, Input, FormGroup, Button, Row} from 'reactstrap';
+
+import placeHolder from './avatarSamples/placeholder.png';
 import Col from 'reactstrap/lib/Col';
 import Form from 'reactstrap/lib/Form';
 
@@ -11,6 +13,7 @@ export default class Home extends React.Component {
             <hr/>
             <h3>Sequence your avatar</h3>
 
+            <Row>
             <Col sm = "6" lg = "6" >
                 <Form>
                     <FormGroup>
@@ -33,9 +36,9 @@ export default class Home extends React.Component {
                     <FormGroup>
                         <Label for="exampleSelect">Hair Texture</Label>
                         <Input type="select" name="select" id="exampleSelect">
-                            <option>Light (TAC CCC GGG)</option>
-                            <option>Medium (TAC CCC AGG)</option>
-                            <option>Dark (TAC CCC AAG)</option>
+                            <option>Curly (CCC GGG AAA)</option>
+                            <option>Wavy (CCC GGG TAA)</option>
+                            <option>Straight (CCC GGG TTA)</option>
                         </Input>
                     </FormGroup>
                     <FormGroup>
@@ -43,15 +46,15 @@ export default class Home extends React.Component {
                         <Input type="select" name="select" id="exampleSelect">
                             <option>Small (TCT TAA AAA)</option>
                             <option>Average (TCT TGA AAA)</option>
-                            <option>Larte (TCT TGG AAC)</option>
+                            <option>Large (TCT TGG AAC)</option>
                         </Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="exampleSelect">Nose Shape</Label>
                         <Input type="select" name="select" id="exampleSelect">
-                            <option>Small (TCT TAA AAA)</option>
-                            <option>Average (TCT TGA AAA)</option>
-                            <option>Larte (TCT TGG AAC)</option>
+                            <option>Pointed (ACC CGA TAT)</option>
+                            <option>Average (ACC CGA TTC)</option>
+                            <option>Rounded (ACC GGA TTC)</option>
                         </Input>
                     </FormGroup>
                     <FormGroup>
@@ -63,16 +66,32 @@ export default class Home extends React.Component {
                         </Input>
                     </FormGroup>
                     <FormGroup>
-                        <Label for="exampleSelect">Nose Shape</Label>
+                        <Label for="exampleSelect">Lip colo</Label>
                         <Input type="select" name="select" id="exampleSelect">
-                            <option>Small (TCT TAA AAA)</option>
-                            <option>Average (TCT TGA AAA)</option>
-                            <option>Larte (TCT TGG AAC)</option>
+                            <option>Purplish (CAG TTT AAA)</option>
+                            <option>Brownish (CAG TAT ACC)</option>
+                            <option>Pinkish (CAG AAT ACA)</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleSelect">Sex</Label>
+                        <Input type="select" name="select" id="exampleSelect">
+                            <option>Male (AAA TTT GCA)</option>
+                            <option>Female (AAA TTT TCC)</option>
                         </Input>
                     </FormGroup>
                 </Form>
                 <Button className = "submit-form" color = "success">Submit</Button>{' '}
             </Col>
+                <Col className = "avatar-col">
+                    <Row>
+                        <img src = {placeHolder}/>
+                    </Row>
+                    <Row className = "avatar-info">
+                        <h3>Your current Avatar</h3>
+                    </Row>
+                </Col>
+            </Row>
         </Container>
         )
     }
